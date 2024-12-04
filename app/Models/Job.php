8471e1dbf4f2dd6b71a41dec4f21c69b904c5d85
Jari Peltola <jena9988@gmail.com>
2024-12-04 13:20:21 +0200

    form and csrf

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,15 +10,16 @@ class Job extends Model {
 
     use HasFactory;
     protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary'];
+    // protected $fillable = ['employer_id', 'title', 'salary'];
+    protected $guarded = [];
 
     public function employer() {
         return $this->belongsTo(Employer::class);
     }
 
-    public function tags() {
-        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
-    }
+    // public function tags() {
+    //     return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+    // }
 
 }
 
